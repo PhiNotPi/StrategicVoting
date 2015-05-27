30beea10a4ba5aff41be8ad26b8252ba001bff18
PhiNotPi <mrclarinetplayer@gmail.com>
2015-05-27 12:20:01 -0400

    fixed spelling

--- Player.java
+++ Player.java
@@ -2,5 +2,5 @@ public interface Player
 {
 	public String getName();
 	public int getVote(int [] voteCounts, int votersRemaining, int [] payoffs, int[] totalPayoffs);
-	public void recieveResults(int[] voteCounts, double result);
+	public void receiveResults(int[] voteCounts, double result);
 }
